ac015fdf44460fd47f5a385809a91437c2887d8a
Augusto Ferreira <gstfrr@gmail.com>
2018-07-15 18:48:19 -0300

    deletar porta do usuario

--- User.py
+++ User.py
@@ -4,19 +4,22 @@ import math
 
 class User:
 
-    def __init__(self, host, port):
+    def __init__(self, host):
         self.__host = host
-        self.__port = port
 
     def send_message(self, message, destination_address):
-        npackets = math.ceil(len(message) / 255)
+        npackets = int(math.ceil(len(message) / 255))
         listpackets = []
         for i in range(npackets):
             listpackets.append(message[i * 255:(i + 1) * 255])
 
         for segment in listpackets:
-            Enlace.data_indication(destination_address,self.__host, segment)
-            #esperar o ack
+            Enlace.data_indication(destination_address, self.__host, segment)
+
+            self.receive_message()
+
 
     def receive_message(self):
+        Enlace.data_request()
+
         pass
