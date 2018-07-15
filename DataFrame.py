2d2c3343576b285b15cb7e4fee69f0df2f3a6c74
Augusto Ferreira <gstfrr@gmail.com>
2018-07-15 14:58:46 -0300

    Classe do quadro de dados

---
+++ DataFrame.py
@@ -0,0 +1,34 @@
+class DataFrame:
+    __delimiter = None
+    __length = None
+    __sequence = None
+    __destination_address = None
+    __source_address = None
+    __payload = None
+    __crc = None
+
+    def __init__(self, length, sequence, destination_address, source_address, payload, crc):
+        self.__delimiter = "01111110"
+        self.__length = length
+        self.__sequence = sequence
+        self.__destination_address = destination_address
+        self.__source_address = source_address
+        self.__payload = payload
+        self.__crc = crc
+
+    def last_bit_sequence(self):
+        return self.__sequence[7]
+
+    def first_bit_sequence(self):
+        return self.__sequence[0]
+
+    def return_data_frame(self):
+        frame = ''
+        frame += self.__delimiter
+        frame += self.__length
+        frame += self.__sequence
+        frame += self.__destination_address
+        frame += self.__source_address
+        frame += self.__payload
+        frame += self.__crc
+        return frame
