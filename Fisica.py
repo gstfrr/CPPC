595042a43fc26269c534a76c9b6bf10739975769
Augusto Ferreira <gstfrr@gmail.com>
2018-07-16 14:46:51 -0300

    Camada física: causa ruidos (troca de bits)

---
+++ Fisica.py
@@ -0,0 +1,27 @@
+import math
+import random
+
+
+class Fisica:
+    __TAXA = 0.1
+
+    @staticmethod
+    def add_noise(msg):
+        size = len(msg)
+        quantidade = math.ceil(Fisica.__TAXA* size)
+
+        for i in range(quantidade):
+            pos = random.randrange(0, len(msg))
+            l = list(msg)
+            l[pos] = Fisica.invert(l[pos])
+            msg = ''.join(l)
+
+
+        return msg
+
+    @staticmethod
+    def invert(char):
+        if char == '1':
+            return '0'
+        else:
+            return '1'
