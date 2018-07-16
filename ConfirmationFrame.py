65c7de6a9df5ce128d98311049fd6b35f7a5879a
Augusto Ferreira <gstfrr@gmail.com>
2018-07-16 20:13:55 -0300

    cliente nada alterado

--- ConfirmationFrame.py
+++ ConfirmationFrame.py
@@ -4,7 +4,7 @@ class ConfirmationFrame:
     __destination_address = None
     __source_address = None
 
-    def __init__(self, __delimiter, sequence, destination_address, source_address):
+    def __init__(self, sequence, destination_address, source_address):
         self.__delimiter = "01111110"
         self.__sequence = sequence
         self.__destination_address = destination_address
@@ -22,3 +22,4 @@ class ConfirmationFrame:
         frame += self.__sequence
         frame += self.__destination_address
         frame += self.__source_address
+        return frame
